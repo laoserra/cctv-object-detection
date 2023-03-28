0b550a228ee6b6e04d2daa90b3d07b76149991a8
Luis Serra <laoserra@gmail.com>
2023-03-28 14:45:37 +0100

    updated query to select data for api

--- select_to_api.py
+++ select_to_api.py
@@ -2,14 +2,12 @@
 
 import psycopg2
 import csv
+import sys
 
 
 conn_str = "host=localhost dbname=detections user=postgres"
 
 def execute_query(query):
-    '''Query database to provide data for the api.
-
-    The model should be set to name of desired model.'''
     try:
         # connect to the PostgreSQL database
         connection = psycopg2.connect(conn_str)
@@ -29,35 +27,30 @@ def execute_query(query):
 
 
 def get_data_api(score, model):
+    '''Query database to provide data for the api.
+
+    The model should be set to name of desired model.'''
+
+    #SQL query
     select_data = f'''
-    SELECT i.unix_time_insertion AS processing_tstz, i.name AS image, 
+    SELECT i.process_tstz, i.image_tstz, i.image_ref, 
            i.warnings, d.class_name, d.score
     FROM images i
     LEFT JOIN detections d
         ON i.id = d.image_id
         AND d.score > {score}
-        AND d.model_id IN (SELECT id FROM models WHERE name = '{model}');
+        AND d.model_id IN (SELECT id FROM models WHERE name = '{model}')
+    WHERE i.image_tstz::date = current_date - INTEGER '1';
     '''
     data = execute_query(select_data)
-    header = [data[0][0], 'image_tstz', 'image_ref']
-    header = header + data[0][2:]
 
-    parsed_data = [tuple(header)]
-    for item in data[1]:
-        splitted_image = item[1].split('_') #returns a list
-        #floor division. Closer to reality due to delay in getting timestamp
-        image_timestamp = int(splitted_image[0]) // 1000
-        image_id = splitted_image[1]
-        new_item = (item[0], image_timestamp, image_id) + item[2:]
-        parsed_data.append(new_item)
+    # prepend header to list of selected records
+    data[1][:0] = [tuple(data[0])]
 
     with open('report.csv', 'w') as fp:
         write = csv.writer(fp)
+        write.writerows(data[1])
 
-        write.writerows(parsed_data)
-
-#Next development step: add another condition to the query, to filter the images captured/processed yesterday. The captured is a bit more tricky because that date is the first part of the image name...
-# Furthermore, maybe I also need to filter all the data for yesterday, including all scores, etc....
 
 if __name__ == '__main__':
-    get_data_api(0.1, 'faster_rcnn_1024_parent')
+    get_data_api(sys.argv[1], sys.argv[2])
