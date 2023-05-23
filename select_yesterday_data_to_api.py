d78fb48c9eedff909ec4d83883506f70307840ad
Luis Serra <laoserra@gmail.com>
2023-05-23 15:27:16 +0100

    Optimized select_yesterday_data script with ChatGPT

--- select_yesterday_data_to_api.py
+++ select_yesterday_data_to_api.py
@@ -3,36 +3,30 @@
 import psycopg2
 import sys
 import pandas as pd
-import gzip
-import datetime as dt
+from datetime import datetime, timedelta
 
 classes_tf2 = ['car', 'person', 'bicycle', 'motorcycle', 'bus', 'truck']
-classes_yolo = ['car', 'pedestrian', 'cyclist', 
+classes_yolo = ['car', 'pedestrian', 'cyclist',
                 'motorcycle', 'bus', 'lorry', 'van', 'taxi']
 
-yesterday = dt.datetime.now() - dt.timedelta(days=1)
-yesterday = yesterday.strftime('%Y%m%d')
+yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y%m%d')
 
 conn_str = "host=localhost dbname=detections user=postgres"
 
 
-def execute_query(query):
+def execute_query(query, params=None):
     try:
         # connect to the PostgreSQL database
-        connection = psycopg2.connect(conn_str)
-        # create new cursor
-        cursor = connection.cursor()
-        cursor.execute(query)
-        header = [descr[0] for descr in cursor.description]
-        result = cursor.fetchall()
-        return header, result #  one tuple with 2 lists(2nd is a list of tuples)
+        with psycopg2.connect(conn_str) as connection:
+            # create new cursor
+            with connection.cursor() as cursor:
+                cursor.execute(query, params)
+                header = [descr[0] for descr in cursor.description]
+                result = cursor.fetchall()
+                # returns one tuple with 2 lists(2nd is a list of tuples)
+                return header, result
     except Exception as error:
         print('Error while connecting to database:', error)
-    finally:
-        # close the communication with the database
-        if connection:
-            cursor.close()
-            connection.close()
 
 
 def select_yesterday_data(score, model):
@@ -41,18 +35,19 @@ def select_yesterday_data(score, model):
        Set desired score threshold and model.'''
 
     # SQL query
-    select_data = f'''
+    select_data = '''
     SELECT i.image_proc, i.image_capt, i.camera_ref, i.warnings, d.class_name,
            d.bbox_left, d.bbox_right, d.bbox_bottom, d.bbox_top, d.score
     FROM images i
     LEFT JOIN detections d
         ON i.id = d.image_id
-        AND d.score > {score}
-        AND d.model_id IN (SELECT id FROM models WHERE name = '{model}')
+        AND d.score > %s
+        AND d.model_id IN (SELECT id FROM models WHERE name = %s)
         AND d.class_name != 'crowd'
-    WHERE i.image_capt::date = current_date - INTEGER '1';
+    WHERE i.image_capt::date = current_date - INTERVAL '1 day'
     '''
-    data = execute_query(select_data)
+    params = (score, model)
+    data = execute_query(select_data, params)
 
     return data
 
@@ -70,7 +65,7 @@ def clean_data(data):
     print('drop duplicates: \n', df.head())
     print('drop duplicates: ', df.shape)
     # remove bboxes columns (only needed to remove duplicates in previous step)
-    df = df[[col for col in df.columns if col[:4] != 'bbox']]
+    df = df[[col for col in df.columns if not col.startswith('bbox')]]
     print('removed bboxes columns: \n', df.head())
 
     return df
@@ -112,26 +107,30 @@ def group_by_class_name(df_clean, model):
 
     if len(df_agg_classes) < len(classes):
         missing_classes = list(set(classes) - set(df_agg_classes))
-        print('missing_classes ', missing_classes )
+        print('missing_classes ', missing_classes)
         for col in missing_classes:
             df_agg[col] = 0
 
-    #concatenate df_nan with df_agg if df_nan existent
+    # concatenate df_nan with df_agg if df_nan existent
     if nan:
         df = pd.concat([df_nan, df_agg])
+        print('concatenated df: \n', df)
         for col in classes:
             df[col] = df[col].fillna(0).astype(int)
     else:
         df = df_agg
-    print('concatenated df: \n', df)
+    print('concatenated df after fillna(0): \n', df)
     # reorder columns
-    columns_order = list(df.columns[:3]) + classes + ['warnings']
-    df = df.reindex(columns = columns_order)
+    columns_order = ['image_proc', 'image_capt', 'camera_ref'] \
+                    + classes \
+                    + ['warnings']
+    df = df.reindex(columns=columns_order)
     # insert name of model
     df.insert(3, 'model_name', model_name)
     print('df with model: \n', df)
 
     return model_name, df
+
 
 def main(score, model):
     data = select_yesterday_data(score, model)
@@ -143,5 +142,5 @@ def main(score, model):
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2])
-    #main(0.5, 'yolov4_9_objs')
-    #get_data_api(0.5, 'faster_rcnn_1024_parent')
+    # main(0.5, 'yolov4_9_objs')
+    # get_data_api(0.5, 'faster_rcnn_1024_parent')
